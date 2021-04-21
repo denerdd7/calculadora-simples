b5b7e55ebb1fc026fc6b57f3a4bf736f1c025095
denerdd7 <76176185+denerdd7@users.noreply.github.com>
2021-04-20 22:10:23 -0300

    Add files via upload

---
+++ Calculadora2/src/calculadora2/Calculadora2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora2;
+import javax.swing.InputMap;
+/**
+ *
+ * @author Use
+ */
+public class Calculadora2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    
+        
+    TelaCalculadora telaCalc = new TelaCalculadora();
+    telaCalc.setLocationRelativeTo(null);
+    telaCalc.setVisible(true);
+    }
+    
+}
